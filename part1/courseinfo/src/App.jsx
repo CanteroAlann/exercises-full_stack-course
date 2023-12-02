40a5e49f636964b32e5d483871d0ab9d966336e3
alan <acantero@fi.uba.ar>
2023-12-01 23:41:12 -0300

    part0 added

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  const { part, exercises } = props;
+  return (
+    <p>{part} {exercises}</p>
+  )
+}
+
+const Content = (props) => {
+  const { parts } = props;
+  return (
+    <>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  const { parts } = props;
+
+  return (
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
